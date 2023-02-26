5779a7109cee880c8bc17c163b3ce0f55342634e
Sebastian Perez <spezuluaga@gmail.com>
2023-02-26 18:34:01 -0500

    meta data added, contact component added and css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
 
 const App = () => {
   return (
@@ -16,8 +17,10 @@ const App = () => {
       <section>
         <Projects />
       </section>
-      {/* <section>Sección de tecnologías</section>
-      <section>Sección de contacto y CV</section> */}
+      {/* <section>Sección de tecnologías</section> */}
+      <section>
+        <Contact />
+      </section>
     </div>
   );
 };
