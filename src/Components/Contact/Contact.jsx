5b6b87040f54a52c34db4ee60490ce2ac39aa096
Sebastian Perez <spezuluaga@gmail.com>
2023-02-27 09:26:47 -0500

    contact section advance

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import LinkedInIcon from '../../assets/linkedin.svg';
+import GitHubIcon from '../../assets/github.svg';
+import GmailIcon from '../../assets/gmail.svg';
 
 const Contact = () => {
   return (
@@ -11,16 +14,43 @@ const Contact = () => {
             aportar contactame
           </p>
         </div>
-        <p>
-          Me puedes escribir directamente a mi correo:{' '}
-          <a href="mailto:spezuluaga@gmail.com">spezuluaga@gmail.com</a>
-        </p>
+        <form
+          action="https://formspree.io/f/mgebyvez"
+          method="POST"
+          className="contact__form"
+        >
+          <label>Tu correo</label>
+          <input type="email" name="email" placeholder="Tu correo" required />
+          <label>Mensaje</label>
+          <textarea name="message" placeholder="Tu mensaje" required />
+          <button type="submit" className="contact__button">
+            Enviar mensaje
+          </button>
+        </form>
         <a href="#">Descarga mi CV</a>
         <div className="contact__socialContainer">
-          <a href="" className="contact__social"></a>
-          <a href="" className="contact__social"></a>
+          <a
+            href="https://github.com/Sebastian-pz"
+            className="contact__social"
+            target="__blank"
+          >
+            <img src={GitHubIcon} alt="Github icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sebastian-perez-zuluaga/"
+            className="contact__social"
+            target="__blank"
+          >
+            <img src={LinkedInIcon} alt="LinkedIn icon" />
+          </a>
+          <a
+            href="mailto:spezuluaga@gmail.com"
+            className="contact__social"
+            target="__blank"
+          >
+            <img src={GmailIcon} alt="Gmail Icon" />
+          </a>
         </div>
-        Descargar CV Ver GitHub Ver LinkedIn Ver correo o un mail to
       </div>
     </div>
   );
